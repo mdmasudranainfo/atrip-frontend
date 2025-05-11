fa0ca7d9c32ba74d33658d87a359c2f9d5212ece
mdmasudranainfo <mdmasude7@gmail.com>
2025-05-11 16:39:02 +0600

    card

--- src/components/layouts/booking-attraction.tsx
+++ src/components/layouts/booking-attraction.tsx
@@ -4,18 +4,18 @@ import { Check } from "lucide-react";
 
 const benefits = [
   "Best price guarantee",
-  "Easy booking process",
-  "Exclusive offer and discount",
-  "Instant confirmation",
-  "24/7 customer support",
+  "24/7 Customer Support",
+  "Easy booking System ",
+  "Secure payment",
+  "Instant Confirmation",
 ];
 
 export default function BookingAttraction() {
   return (
     <div className="border rounded-lg shadow-sm p-4 mt-4 max-w-sm bg-white">
       <h6 className="text-lg mb-3">
-        Top 5 reasons to book holiday from{" "}
-        <span className="font-bold">atrip.ae</span>
+        Travelers should choose <span className="font-bold">atrip.ae</span>{" "}
+        because of these following top 5 benefits.
       </h6>
       <ul className="space-y-2">
         {benefits.map((benefit, index) => (
