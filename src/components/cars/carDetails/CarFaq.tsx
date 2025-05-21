b1a816a48d43cba9fa4b71bf59536185e376b12a
mdmasudranainfo <mdmasude7@gmail.com>
2025-05-21 10:12:22 +0600

    accordion

--- src/components/cars/carDetails/CarFaq.tsx
+++ src/components/cars/carDetails/CarFaq.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { HelpCircle } from "lucide-react";
 import {
   Accordion,
@@ -13,35 +15,29 @@ export default function CarFAQs({
   faqs: { title: string; content: string }[];
 }) {
   return (
-    <Card className="w-full border-none none rounded-none">
-      <Accordion type="single" value="header" collapsible>
-        <AccordionItem value="header" className="border-0">
-          <AccordionTrigger className="px-6 py-4 hover:no-underline">
-            <div className="flex items-center gap-2">
-              <HelpCircle className="h-5 w-5 text-blue-600" />
-              <span className="text-base font-bold">Travelers Asking</span>
-            </div>
-          </AccordionTrigger>
-          <AccordionContent className="px-6 pb-6">
-            <Accordion type="single" collapsible className="w-full">
-              {faqs?.map((faq, index) => (
-                <AccordionItem
-                  key={index}
-                  value={`item-${index}`}
-                  className="border-0"
-                >
-                  <AccordionTrigger className="py-4 text-sm font-semibold hover:no-underline">
-                    {faq?.title}
-                  </AccordionTrigger>
-                  <AccordionContent className="text-sm font-normal">
-                    {faq?.content}
-                  </AccordionContent>
-                </AccordionItem>
-              ))}
-            </Accordion>
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-    </Card>
+    <div className="w-full bg-white border-t-2 py-8">
+      <div className="px-4">
+        <div className="flex items-center gap-2 mb-6 px-2">
+          <HelpCircle className="h-10 w-10 text-blue-600 bg-blue-50 p-2 rounded-lg" />
+          <h3 className="text-md font-bold text-[#1A1A1A]">Travelers Asking</h3>
+        </div>
+        <Accordion type="single" collapsible className="space-y-4">
+          {faqs?.map((faq, index) => (
+            <AccordionItem
+              key={index}
+              value={`item-${index}`}
+              className="rounded-2xl bg-white shadow border border-gray-100"
+            >
+              <AccordionTrigger className="flex justify-between items-center px-6 py-4 text-base font-semibold text-[#1A1A1A] hover:bg-blue-50 rounded-2xl transition">
+                {faq?.title}
+              </AccordionTrigger>
+              <AccordionContent className="px-6 pb-4 text-gray-600 text-[15px] leading-relaxed">
+                {faq?.content}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
+    </div>
   );
 }
